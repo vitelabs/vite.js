f9018caf544fbbad4b0fd796a24f9e0af590c9c7
lovelycs <419579547@qq.com>
2018-09-26 19:14:23 +0800

    test case

--- test/utils.js
+++ test/utils.js
@@ -3,18 +3,15 @@ import utils from '../libs/utils.js';
 
 describe('strToUtf8', function () {
     it('test1', function () {
-        let utf8Bytes = utils.strToUtf8('为', true);
-        let bytes = new Uint8Array(utf8Bytes);
-        assert.equal('e4b8ba', utils.bytesToHex(bytes));
+        let utf8Bytes = utils.strToUtf8Bytes('为', true);
+        assert.equal('e4b8ba', utils.bytesToHex(utf8Bytes));
     });
     it('test2', function () {
-        let utf8Bytes = utils.strToUtf8('sdsdsds。；。、lp.;,p[oo阿京东方]', true);
-        let bytes = new Uint8Array(utf8Bytes);
-        console.log(utils.bytesToHex(bytes));
+        let utf8Bytes = utils.strToUtf8Bytes('sdsdsds。；。、lp.;,p[oo阿京东方]', true);
+        console.log(utils.bytesToHex(utf8Bytes));
     });
     it('test3', function () {
-        let utf8Bytes = utils.strToUtf8('[坏笑]😊🙂🙂😆😅😅', true);
-        let bytes = new Uint8Array(utf8Bytes);
-        console.log(utils.bytesToHex(bytes));
+        let utf8Bytes = utils.strToUtf8Bytes('[坏笑]😊🙂🙂😆😅😅', true);
+        console.log(utils.bytesToHex(utf8Bytes));
     });
 });
