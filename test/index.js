e06242764c950f4254d5b201b9fef2f7c17e1452
lovelycs <419579547@qq.com>
2018-09-28 15:16:54 +0800

    test case

--- test/index.js
+++ test/index.js
@@ -1,27 +1,27 @@
-// describe('Mnemonic Test: bip39', function () {
-//     require('./mnemonic');
-// });
+describe('Mnemonic Test: bip39', function () {
+    require('./mnemonic');
+});
 
-// describe('HD Test: libs/hd', function () {
-//     require('./hd');
-// });
+describe('HD Test: libs/hd', function () {
+    require('./hd');
+});
 
-// describe('Ed25519 blake2b Test: tweetnale-blake2b', function () {
-//     require('./ed25519');
-// });
+describe('Ed25519 blake2b Test: tweetnale-blake2b', function () {
+    require('./ed25519');
+});
 
-// describe('Address Test: src/utils/address', function () {
-//     require('./address');
-// });
+describe('Address Test: src/utils/address', function () {
+    require('./address');
+});
 
-// describe('Vite Test: src/Vite', function () {
-//     require('./Vite/account');
-// });
+describe('Vite Test: src/Vite', function () {
+    require('./Vite/account');
+});
 
 describe('Wallet Test: src/Wallet', function () {
-    // require('./Wallet/keystore');
+    require('./Wallet/keystore');
     require('./Wallet/address');
-    // require('./Wallet/account');
+    require('./Wallet/account');
 });
 
 describe('Utils Test: libs/utils', function () {
