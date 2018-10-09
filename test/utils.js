78449aa3f97b879596d2e12bb8e2367f7b0dfd1f
lovelycs <419579547@qq.com>
2018-10-09 15:17:49 +0800

    test all

--- test/utils.js
+++ test/utils.js
@@ -6,12 +6,12 @@ describe('strToUtf8', function () {
         let utf8Bytes = utils.strToUtf8Bytes('为', true);
         assert.equal('e4b8ba', utils.bytesToHex(utf8Bytes));
     });
-    it('test2', function () {
-        let utf8Bytes = utils.strToUtf8Bytes('sdsdsds。；。、lp.;,p[oo阿京东方]', true);
-        console.log(utils.bytesToHex(utf8Bytes));
-    });
-    it('test3', function () {
-        let utf8Bytes = utils.strToUtf8Bytes('[坏笑]😊🙂🙂😆😅😅', true);
-        console.log(utils.bytesToHex(utf8Bytes));
-    });
+    // it('test2', function () {
+    //     let utf8Bytes = utils.strToUtf8Bytes('sdsdsds。；。、lp.;,p[oo阿京东方]', true);
+    //     console.log(utils.bytesToHex(utf8Bytes));
+    // });
+    // it('test3', function () {
+    //     let utf8Bytes = utils.strToUtf8Bytes('[坏笑]😊🙂🙂😆😅😅', true);
+    //     console.log(utils.bytesToHex(utf8Bytes));
+    // });
 });
