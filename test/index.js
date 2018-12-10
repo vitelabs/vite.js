7e8ec4cb4d4da28709e11581021057a4d5b7f76d
lovelycs <419579547@qq.com>
2018-12-10 21:19:09 +0800

    add test case

--- test/index.js
+++ test/index.js
@@ -1,12 +1,12 @@
-// describe('Mnemonic Test: bip39', function () {
-//     require('./bip39');
-// });
+describe('Mnemonic Test: bip39', function () {
+    require('./bip39');
+});
 
 describe('Utils Test: src/utils', function () {
-    // require('./utils/address');
-    // require('./utils/keystore');
+    require('./utils/address');
+    require('./utils/keystore');
     require('./utils/accountBlock');
-    // require('./utils/ed25519');
-    // require('./utils/encoder');
-    // require('./utils/tools');
+    require('./utils/ed25519');
+    require('./utils/encoder');
+    require('./utils/tools');
 });
