87f87bfee311137600aca6ebdb99079dd3a2e429
lovelycs <419579547@qq.com>
2019-02-09 14:05:32 +0800

    abi coder

--- test/index.js
+++ test/index.js
@@ -1,13 +1,13 @@
-// describe('Mnemonic Test: bip39', function () {
-//     require('./bip39');
-// });
+describe('Mnemonic Test: bip39', function () {
+    require('./bip39');
+});
 
 describe('Utils Test: src/utils', function () {
-    // require('./utils/address');
-    // require('./utils/keystore');
-    // require('./utils/accountBlock');
-    // require('./utils/ed25519');
-    // require('./utils/encoder');
-    // require('./utils/tools');
+    require('./utils/address');
+    require('./utils/keystore');
+    require('./utils/accountBlock');
+    require('./utils/ed25519');
+    require('./utils/encoder');
+    require('./utils/tools');
     require('./utils/abi');
 });
