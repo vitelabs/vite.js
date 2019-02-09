1cf1ffa4778abce9fd14f2382a5f489233094ba8
lovelycs <419579547@qq.com>
2019-02-09 14:18:13 +0800

    add abi

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,6 +4,7 @@ import * as e from "./encoder";
 import * as k from "./keystore";
 import * as _e from "./ed25519";
 import * as accB from "./accountBlock";
+import * as _abi from "./abi";
 
 export const tools = t;
 export const address = a;
@@ -11,3 +12,4 @@ export const encoder = e;
 export const keystore = k;
 export const ed25519 = _e;
 export const accountBlock = accB;
+export const abi = _abi;
