53ebdeba2d5e0c42facc032467bde32226fddcc9
hurrytospring <ddyz1993@163.com>
2019-02-28 12:22:34 +0800

    add stri

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,5 +1,18 @@
 const blake = require('blakejs/blake2b');
 import { paramsMissing, paramsFormat } from "const/error";
+import qs from 'qs';
+
+export function uriStringify(o:{schema:String,prefix:String,targetAddress:String,chain_id:Number,function_name:String,params:Object}) {
+    const {schema,prefix,targetAddress,chain_id,function_name,params}=o;
+    const _schema=schema?`${schema}:`:'vite:';
+    const _prefix=prefix===undefined?'':`${prefix}-`;
+    const _targetAddress=targetAddress||'';
+    const _chain_id=chain_id?`@${chain_id}`:'';
+    const _function_name=function_name?`\\${function_name}`:'';
+    const _params=params?`?${qs.stringify(params,{encode:false})}`:'';
+    const str=`${_schema}${_prefix}${_targetAddress}${_chain_id}${_function_name}${_params}`;
+    return str;
+}
 
 export function checkParams(params, requiredP:Array<string> = [], validFunc:Array<{ name, func, msg? }> =[]) {
     if (!params) {
