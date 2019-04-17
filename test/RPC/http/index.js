b864422c734e865eb5b3f37c13e8609ef303bcbb
lovelycs <419579547@qq.com>
2019-04-17 13:00:55 +0800

    test http

--- test/RPC/http/index.js
+++ test/RPC/http/index.js
@@ -2,6 +2,7 @@ import HTTP_RPC from '../../../src/HTTP';
 import Client from '../../../src/client/index';
 import HdAccount from '../../../src/hdAccount/index';
 import Account from '../../../src/account/index';
+import { bytesToHex, blake2b, hexToBytes } from '../../../src/utils/index';
 
 const myHTTPClient = new Client(new HTTP_RPC('http://148.70.30.139:48132'));
 const myHdAccount = new HdAccount({
@@ -9,12 +10,8 @@ const myHdAccount = new HdAccount({
     client: myHTTPClient
 });
 
-// console.log(myHTTPClient);
-// console.log(myHdAccount);
-
-// console.log(myHTTPClient.request);
-
 const address = myHdAccount.addrList[0].hexAddr;
+const realAddr = myHdAccount.addrList[0].addr;
 const privateKey = myHdAccount.addrList[0].privKey;
 
 // const activeAccount = myHdAccount.activateAccount({}, {
@@ -31,13 +28,43 @@ const myAccount = new Account({
 myHTTPClient.onroad.getOnroadBlocksByAddress(address, 0, 10).then(data => {
     console.log('getOnroadBlocksByAddress', data);
 
+    if (!data || !data.length) {
+        return;
+    }
+
     const block = data[0];
 
-    // myHTTPClient.tx.calcPoWDifficulty()
+    myHTTPClient.buildinTxBlock.asyncReceiveTx({
+        accountAddress: address,
+        fromBlockHash: block.hash
+    }).then(accBlock => {
+        // const prevHash = Number(accBlock.prevHash) === 0 ? '0' : accBlock.prevHash;
+        myHTTPClient.tx.calcPoWDifficulty({
+            selfAddr: address,
+            prevHash: accBlock.prevHash,
+            blockType: accBlock.blockType,
+            toAddr: accBlock.toAddress,
+            data: accBlock.data,
+            usePledgeQuota: true
+        }).then(data => {
+            const hash = bytesToHex(blake2b(hexToBytes(realAddr + accBlock.prevHash), null, 32));
 
-
-    myAccount.receiveTx(block.hash).then(data => {
-        console.log(data);
+            if (data.difficulty) {
+                myHTTPClient.request('pow_getPowNonce', data.difficulty, hash).then(nonce => {
+                    accBlock.nonce = nonce;
+                    accBlock.difficulty = data.difficulty;
+                    myAccount.sendRawTx(accBlock).then(data => {
+                        console.log(data);
+                    }).catch(err => {
+                        console.log(err);
+                    });
+                }).catch(err => {
+                    console.log(err);
+                });
+            }
+        }).catch(err => {
+            console.log(err);
+        });
     }).catch(err => {
         console.log(err);
     });
@@ -46,19 +73,19 @@ myHTTPClient.onroad.getOnroadBlocksByAddress(address, 0, 10).then(data => {
 });
 
 
-// myHTTPClient.getBalance(address).then(data => {
-//     console.log('getBalance', data);
-// }).catch(err => {
-//     console.log('getBalance', err);
-// });
+myHTTPClient.getBalance(address).then(data => {
+    console.log('getBalance', data);
+}).catch(err => {
+    console.log('getBalance', err);
+});
 
-// myHTTPClient.getTxList({
-//     addr: address,
-//     index: 0,
-//     pageCount: 10
-// }).then(data => {
-//     console.log('getTxList', data);
-// }).catch(err => {
-//     console.log('getTxList', err);
-// });
+myHTTPClient.getTxList({
+    addr: address,
+    index: 0,
+    pageCount: 10
+}).then(data => {
+    console.log('getTxList', data);
+}).catch(err => {
+    console.log('getTxList', err);
+});
 
--- test/cover.js
+++ test/cover.js
@@ -1,27 +1,27 @@
-// describe('Abi Test: src/abi', function () {
-//     require('./abi');
-// });
+describe('Abi Test: src/abi', function () {
+    require('./abi');
+});
 
 describe('AccountBlock Test: src/accountBlock', function () {
     require('./accountBlock');
 });
 
-// describe('Constant Test: src/constant', function () {
-//     require('./constant');
-// });
+describe('Constant Test: src/constant', function () {
+    require('./constant');
+});
 
-// describe('HdAddr Test: src/hdAddr', function () {
-//     require('./hdAddr');
-// });
+describe('HdAddr Test: src/hdAddr', function () {
+    require('./hdAddr');
+});
 
-// describe('Keystore Test: src/keystore', function () {
-//     require('./keystore');
-// });
+describe('Keystore Test: src/keystore', function () {
+    require('./keystore');
+});
 
-// describe('PrivToAddr Test: src/privToAddr', function () {
-//     require('./privToAddr');
-// });
+describe('PrivToAddr Test: src/privToAddr', function () {
+    require('./privToAddr');
+});
 
-// describe('Utils Test: src/utils', function () {
-//     require('./utils/index');
-// });
+describe('Utils Test: src/utils', function () {
+    require('./utils/index');
+});
