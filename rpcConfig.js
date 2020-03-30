f814d20f539b25112eba2e62eba881950340d696
soliury <soliury@pm.me>
2020-03-30 14:43:40 +0800

    ci: Add rpcConfig.js

---
+++ rpcConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+    ws: 'wss://api.vitewallet.com/ws',
+    http: 'https://api.vitewallet.com/http',
+    mnemonic: 'tomorrow practice bulb brown album saddle track random stem giggle dentist language brown sunset aunt absurd boat track green source fresh method bike athlete',
+    addrIndex: 1,
+    blockList: [],
+    myMnemonic: 'tomorrow practice bulb brown album saddle track random stem giggle dentist language brown sunset aunt absurd boat track green source fresh method bike athlete'
+}
