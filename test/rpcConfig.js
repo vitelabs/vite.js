15f8e7018b7428bcc57419d40f6f3db6f2294e05
soliury <soliury@pm.me>
2020-06-25 15:24:01 +0800

    test: change rpcconfig

--- test/rpcConfig.js
+++ test/rpcConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    ws: 'wss://api.vitewallet.com/ws',
-    http: 'https://api.vitewallet.com/http',
+    ws: 'wss://api.vitewallet.com/test/ws',
+    http: 'https://api.vitewallet.com/test/ios',
     mnemonic: 'tomorrow practice bulb brown album saddle track random stem giggle dentist language brown sunset aunt absurd boat track green source fresh method bike athlete',
     addrIndex: 1,
     blockList: [],
